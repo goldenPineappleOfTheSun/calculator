550034b81a97cb8e7c0b4d16df76ab3be35441b9
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2018-02-06 15:29:49 +0400

    Add IOperation

---
+++ ConsoleCalc/ItUniver.Calc.Core/Interfaces/IOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItUniver.Calc.Core.Interfaces
+{
+    public interface IOperation
+    {
+        int argsCount { get; }
+
+        double Exec(double[] args);
+
+        string Name { get; }
+    }
+}
